ef363d08af3a4f7b0d882e2fd4c7966f50025c71
dridihaythem <haithemdridiweb@gmail.com>
2022-06-21 10:28:05 +0100

    datatable ar

--- resources/views/partials/admin/datatable.blade.php
+++ resources/views/partials/admin/datatable.blade.php
@@ -1,6 +1,17 @@
 @push('css')
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css" />
+
+<style>
+    .div.dataTables_wrapper,
+    div.dataTables_filter {
+        text-align: left !important;
+    }
+
+    .dataTables_length {
+        float: right;
+    }
+</style>
 @endpush
 @push('js')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script>
@@ -11,6 +22,7 @@ $(document).ready(function(){
     $('.datatable').DataTable({
         pageLength: 10,
         responsive: true,
+        oLanguage: { sUrl: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/ar.json'}
     });
 });
 </script>
