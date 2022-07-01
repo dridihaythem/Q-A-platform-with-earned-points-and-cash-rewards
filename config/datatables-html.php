229d707ffad37e0344b5cf96d3dd0659883f8747
dridihaythem <haithemdridiweb@gmail.com>
2022-07-01 20:58:38 +0100

    Install databales

---
+++ config/datatables-html.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    /*
+     * DataTables JavaScript global namespace.
+     */
+
+    'namespace' => 'LaravelDataTables',
+
+    /*
+     * Default table attributes when generating the table.
+     */
+    'table' => [
+        'class' => 'table table-striped table-bordered table-hover  dataTable no-footer',
+        'id'    => 'table',
+    ],
+
+    /*
+     * Html builder script template.
+     */
+    'script' => 'datatables::script',
+
+    /*
+     * Html builder script template for DataTables Editor integration.
+     */
+    'editor' => 'datatables::editor',
+];
