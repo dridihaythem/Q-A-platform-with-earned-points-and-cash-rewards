3ffe9d3e3fe81abb81652a47d003094bc33bb7eb
dridihaythem <haithemdridiweb@gmail.com>
2022-07-02 09:18:44 +0100

    Fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-secondary py-3 text-white">
     <div class="container text-center">
         <div class="mb-2">
-            <a href="{{ route('pages.privacy') }}" class="text-white text-decoration-none me-2">سياسة الخصوية</a>
+            <a href="{{ route('pages.privacy') }}" class="text-white text-decoration-none me-2">سياسة الخصوصية</a>
             <a href="{{ route('pages.terms') }}" class="text-white text-decoration-none me-2">إتصل بنا</a>
         </div>
         جميع الحقوق محفوظة © 2022
